87982da2a4d792f2282dde9c4cc46f4988896df0
Rodrigo Alba <rodrigo@megabadvisors.com>
2023-01-31 18:17:52 -0400

    migrations done

--- db/migrate/20230130190051_create_reviews.rb
+++ db/migrate/20230130190051_create_reviews.rb
@@ -1,6 +1,10 @@
 class CreateReviews < ActiveRecord::Migration[7.0]
   def change
     create_table :reviews do |t|
+      t.string :comment
+      t.integer :star_rating
+      t.belongs_to :user, null: false, foreign_key: true
+      t.belongs_to :product, null: false, foreign_key: true
 
       t.timestamps
     end
